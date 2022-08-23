53733a1fd59ba50632ae77ef724bb552da218502
Nico <nicosurguladze@gmail.com>
2022-08-23 14:31:24 +0200

    Updates

---
+++ precache-manifest.24f4f58aa1886873b3d7b4cf3798bab9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5484b746b73e5a64472",
+    "url": "/Task-app/static/css/main.7799cf9b.chunk.css"
+  },
+  {
+    "revision": "d5484b746b73e5a64472",
+    "url": "/Task-app/static/js/main.d5484b74.chunk.js"
+  },
+  {
+    "revision": "c6b5a3e0f92ee25b3986",
+    "url": "/Task-app/static/js/1.c6b5a3e0.chunk.js"
+  },
+  {
+    "revision": "17edaaa9186f1c4d7f3c",
+    "url": "/Task-app/static/css/2.a67e0c47.chunk.css"
+  },
+  {
+    "revision": "17edaaa9186f1c4d7f3c",
+    "url": "/Task-app/static/js/2.17edaaa9.chunk.js"
+  },
+  {
+    "revision": "fdaa8bd96d6eaed2432f",
+    "url": "/Task-app/static/js/runtime~main.fdaa8bd9.js"
+  },
+  {
+    "revision": "d577619b8c0c4380ae7217b2c077153a",
+    "url": "/Task-app/index.html"
+  }
+];
